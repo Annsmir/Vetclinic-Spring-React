f9d953789ccd302bb640de9b3b6f968db8f285a2
Anna <Annushka0405@mail.ru>
2023-04-17 16:06:53 +0300

    final exception fix

--- vet — копия (2)/src/main/java/com/example/vet/exception/OwnerNotFoundAdvice.java
+++ vet — копия (2)/src/main/java/com/example/vet/exception/OwnerNotFoundAdvice.java
@@ -1,6 +1,7 @@
 package com.example.vet.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import java.util.HashMap;
 import java.util.Map;
 
+@ControllerAdvice
 public class OwnerNotFoundAdvice {
     @ResponseBody
     @ExceptionHandler(OwnerNotFoundException.class)
